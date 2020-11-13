a76c29b53cdb10dbd33dc3887dd05a37246aa704
rekharamesh <meetreks@gmail.com>
2020-11-13 19:34:18 +0000

    Om Sai Ram Java Check

---
+++ src/main/java/Sairam.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Sairam {
+    public static void main(String[] args) throws IOException {
+
+
+        System.out.println("OmSairam - Gradle");
+
+    }
+}
